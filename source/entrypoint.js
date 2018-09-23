73c23abb415489ef971c00a1aa6d040d6a26c43c
Dr. Safi <sfmissive@gmail.com>
2018-09-23 21:06:56 +0300

    ✒ Unit tests, &some clarifying comments.

--- source/entrypoint.js
+++ source/entrypoint.js
@@ -1,23 +1,28 @@
-import path from 'path'
+import path, { join } from 'path'
 import { constants as filesystemConstants, promises as filesystem } from 'fs'
 import { recursiveCreateDirectory } from '@dependency/recursiveCreateDirectory'
 
 async function moduleScopePattern({
-    rootScopeModulePath, // full path of the root scope module i.e. <path to node_modules>/<folders to scope module>
+    rootScopeModulePath, // destination path - full path of the root scope module i.e. <path to node_modules>/<folders to scope module>
+    rootFolderArray, // folders to be symlinked to the module scope folder (paths relative to rootPath).
     rootPath, // path to the project root folder where files should be symlinked from.
-    rootFolderArray // root folders to be symlinked to the module scope folder.
 } = {}) {
     
     console.group(`\x1b[2m\x1b[3m%s`, '• Setup container environment - root scope pattern:')
-
+    
     rootScopeModulePath = rootScopeModulePath; // scope module for top-level directory symlinks
-    await filesystem.access(rootScopeModulePath, filesystemConstants.F_OK) // if directory doesn't exist
+    
+    // if directory doesn't exist
+    await filesystem.access(rootScopeModulePath, filesystemConstants.F_OK) 
                     .catch(error => recursiveCreateDirectory({ directoryPath: rootScopeModulePath }))
 
-    for (let folderName of rootFolderArray) { // symlink each folder from root path to module scope path.
-        await filesystem.symlink(`${rootPath}/${folderName}`, `${rootScopeModulePath}/${folderName}`)
+    // symlink each folder from root path to module scope path.
+    for (let folderName of rootFolderArray) {
+        let folderSourcePath = path.join(rootPath, folderName)
+        let folderDestinationPath = path.join(rootScopeModulePath, folderName)
+        await filesystem.symlink(folderSourcePath, folderDestinationPath)
                         .catch( error => console.log(`Symlink created for ${folderName}`) )
-    }    
+    }
 
     console.groupEnd()
 }
